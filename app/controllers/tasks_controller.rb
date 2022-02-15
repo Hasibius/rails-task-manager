3054632b9762eb50e932a3dab21258343ddb4912
Hasib Selimovic <hasib.selimovic@gmail.com>
2022-02-15 16:23:41 +0100

    some more steps

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,26 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    # get data from the form (params)
+    # create a new Restaurant object/instance with that data!
+    task = Task.new(tasks_params)
+    # save it
+    task.save
+    # go somewhere...?
+    redirect_to task_path(task)
+  end
+
+  def edit; end
+
+  def update
+    # find the restaurant by its id
+    # update it with the data from params
+    # redirect to show page for the restaurant
+    @task.update(tasks_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
   def tasks_params
